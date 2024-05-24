10c81c00531cf4582894ef315b044dd993746bf8
Nikita <nikita.voloboev@gmail.com>
2024-05-24 15:06:15 +0200

    .

--- react-native-ide-expo/.eslintrc.js
+++ react-native-ide-expo/.eslintrc.js
@@ -1,8 +1,4 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-	extends: ["expo", "prettier"],
-	plugins: ["prettier"],
-	rules: {
-		"prettier/prettier": "error",
-	},
+	extends: ["expo"],
 }
