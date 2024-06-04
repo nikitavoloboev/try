c5bcb2d33ac8b742886c3a07b62c5ca0120df104
Nikita <nikita.voloboev@gmail.com>
2024-06-04 14:28:56 +0300

    .

---
+++ react-vite/src/App.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+
+export default function App() {
+	const { isPending, error } = useQuery({
+		queryKey: ["home"],
+		queryFn: () =>
+			fetch("https://nikiv-drophunt.web.val.run").then((res) => res.json()),
+	})
+
+	if (isPending) return "Loading..."
+
+	if (error) return "An error has occurred: " + error.message
+
+	// return <div>{JSON.stringify(data)}</div>
+	return <div></div>
+}
