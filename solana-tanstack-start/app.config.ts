9d8b70b4eba1631753646c11033c50f64fb99e41
Nikita <github@nikiv.dev>
2024-10-15 12:46:37 +0200

    failing still

--- solana-tanstack-start/app.config.ts
+++ solana-tanstack-start/app.config.ts
@@ -1,31 +1,45 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
-import { defineConfig } from "@tanstack/start/config"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
-import tsConfigPaths from "vite-tsconfig-paths"
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
+// TODO: failing with `Cannot read properties of undefined (reading 'server')`
 export default defineConfig({
-  routers: {
-    client: {
-      vite: {
-        plugins: () => [nodePolyfills()],
-        optimizeDeps: {
-          esbuildOptions: {
-            plugins: [
-              // @ts-ignore
-              NodeGlobalsPolyfillPlugin({
-                process: true,
-              }),
-            ],
-          },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          coral: ["@coral-xyz/anchor"],
+          jotai: ["jotai"],
+          react: ["react", "react-dom"],
+          reactHotToast: ["react-hot-toast"],
+          reactRouter: ["react-router", "react-router-dom"],
+          solanaWeb3: ["@solana/web3.js"],
+          solanaWalletAdapters: [
+            "@solana/wallet-adapter-base",
+            "@solana/wallet-adapter-react",
+            "@solana/wallet-adapter-react-ui",
+          ],
+          tabler: ["@tabler/icons-react"],
+          tanstack: ["@tanstack/react-query"],
         },
       },
     },
   },
-  vite: {
-    plugins: () => [
-      tsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
+  define: {
+    global: "globalThis",
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // @ts-ignore
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+        }),
+      ],
+    },
+  },
+  plugins: [viteTsconfigPaths(), react(), nodePolyfills()],
 })
