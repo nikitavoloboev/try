4052d6a0e8b4fae17828480e81e93be54544b075
Nikita <github@nikiv.dev>
2025-03-23 00:15:51 +0100

    .

--- next-intl-next/src/app/[locale]/not-found.tsx
+++
@@ -1,4 +0,0 @@
-// Note that `app/[locale]/[...rest]/page.tsx`
-// is necessary for this page to render.
-
-export {default} from '@/components/NotFoundPage'
--- next-intl-next/src/i18n/navigation.ts
+++
@@ -1,5 +0,0 @@
-import {createNavigation} from 'next-intl/navigation'
-import {routing} from './routing'
-
-export const {Link, getPathname, redirect, usePathname, useRouter} =
-  createNavigation(routing)
